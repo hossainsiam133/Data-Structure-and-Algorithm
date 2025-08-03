a8b6042286e5dede476eea0187ab98a3a5f08ecc
hossainsiam133 <hsiam133946@gmail.com>
2025-08-03 22:10:42 +0600

    Updated Commit

--- test.py
+++
@@ -1,6 +0,0 @@
-for i in range(1, 8):
-    with open(
-        f"/home/siam-hossain/Documents/Data-Structure-and-Algorithm/Tutorial/Chapter {i}.md",
-        "w",
-    ) as file:
-        file.write("")
